8889e2ef94cf3b1e1399b5d9dc9ad907fff6b6f0
Athari31 <138482333+Athari31@users.noreply.github.com>
2023-08-23 12:19:36 +0400

    Initial commit

---
+++ bin/dart_application_16.dart
@@ -0,0 +1,40 @@
+import 'dart:ffi';
+
+import 'package:dart_application_16/dart_application_16.dart' as dart_application_16;
+import 'dart:io';
+// void main (){}
+// // no return - no param
+// void printAnyThings(){
+//   print("Hello");
+// }
+// // no return - no param
+// String getName () {
+//   return "Athari";
+// }
+// // no return - no param
+// int getnumber(){
+//   return 1+2;
+// }
+// void call (int x , int y){
+//  print(x+y);
+// }
+// void setName (String name){
+//   print(name);
+// }
+
+void main(){
+calc2(
+  name:"Athari"
+);
+}
+
+void calc2({required String name}){
+  print(name);
+}
+calc3(
+  name:"Athari"
+);
+void calc3({String? name}){
+  print(name);
+}
+
